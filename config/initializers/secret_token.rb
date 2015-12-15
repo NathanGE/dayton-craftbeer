d0693723876e18b3cf153c5fe3978d9f5aa515a8
Nathan <nathangelliott@gmail.com>
2015-12-15 13:32:57 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DaytonCraftbeer::Application.config.secret_key_base = 'eacac50a70c1f1d1a81451277100121a422de3d87d814ebae29319b255c45e27f055e662d02390f861fdbb3b7a5e4aa828c07212487df976b6a02c8b7b6ade4e'
