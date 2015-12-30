05c1b1821466c46c57bbf32f4805c9baa66252cb
Nathan <nathangelliott@gmail.com>
2015-12-30 01:58:26 +0000

    Updated automated notification email mailer - references Firehose Project, Nomster, Lesson 42

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,6 +1,8 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@dayton-craftbeer.herokuapp.com"
-  def comment_added
-    mail(to: "nathangelliott@gmail.com", subject: "A comment has been added to the place you created on Dayton-Craftbeer.herokuapp.com.")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email, subject: "A comment has been added to #{@place.name} on Dayton-Craftbeer.herokuapp.com.")
   end
 end
