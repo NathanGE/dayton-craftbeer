efc0bed3fd363043b8c6ad61ce2ad425bb5caaf5
Nathan <nathangelliott@gmail.com>
2016-01-06 00:36:21 +0000

    Added test for creating a comment on comment controller

--- test/factories.rb
+++ test/factories.rb
@@ -7,7 +7,18 @@ end
 
 FactoryGirl.define do
   factory :user do
-    email "test@test.com"
+    sequence :email do |n|
+      "me#{n}@email.com"
+    end
     password "password"
+  end
+end
+
+FactoryGirl.define do
+  factory :place do
+    name "test place"
+    description "test description"
+    address "123 test st, test, test 12345"
+    association :user
   end
 end
