a8e5ac2dff5bb77a416638ad8e0c1faeba0561a4
Nathan <nathangelliott@gmail.com>
2016-01-12 15:36:24 +0000

    Added tests to places controller

--- test/controllers/places_controller_test.rb
+++ test/controllers/places_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class PlacesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    FactoryGirl.create(:place)
+    get :index
+    assert_response :success    
+  end
+
+  test "show found place" do
+    place = FactoryGirl.create(:place)
+    get :show, id: place.id
+    assert_response :success
+  end
+
+  test "new" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    get :new
+    assert_response :success
+  end
+
+  test "new not signed in" do
+    get :new
+    assert_redirected_to new_user_session_path
+  end
+
 end
+
+
