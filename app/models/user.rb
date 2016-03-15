57ba56ef18a141932326c5b81099801542c997cd
Nathan <nathangelliott@gmail.com>
2016-03-15 20:48:19 +0000

    Added username to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,19 @@ class User < ActiveRecord::Base
 
   has_many :places
   has_many :comments
+
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_hash).first
+    end
+  end
+
+  validates :username, presence: true, length: {maximum: 255}, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]*\z/, message: "may only contain letters and numbers." }
 end
